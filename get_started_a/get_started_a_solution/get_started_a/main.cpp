b24baa017d2cf9370d8cd0f4e7f3a66e2b800fe0
GunnarGramser <40443243+GunnarGramser@users.noreply.github.com>
2023-03-24 09:57:11 +0100

    get_started_a
    Done exerxize get started a

--- get_started_a/get_started_a_solution/get_started_a/main.cpp
+++ get_started_a/get_started_a_solution/get_started_a/main.cpp
@@ -13,6 +13,20 @@ using namespace std;
 
 int main()
  {
+	int score = 200;
+	int *my_pointer = &score;
+	printf("Score is: %d\n", score);
+	printf("Pointer is: %p\n", my_pointer);
+
+	int &other_score = score;
+	other_score = 800; // shanges the value of score
+	printf("\nScore is: %d\n", score);
+	printf("Other score is: %p\n", my_pointer); // points to same address
+
+	/*
+		Any change to a reference also changes the actual value of the variable
+		it points to!
+	*/
 
 	cout << "\n\n";
 	return 0;
